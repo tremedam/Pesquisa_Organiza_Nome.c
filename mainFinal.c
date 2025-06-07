13a3ab2d014124a7902fa8dc6dc1bb9691d2c1dc
Thiago Dias <thiagofellipe055@gmail.com>
2025-06-07 11:53:33 -0300

    Alterações Final

--- mainFinal.c
+++ mainFinal.c
@@ -1,59 +1,91 @@
-/* Pesquisa */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+void entradaNomes(char nomes[10][40]) {
+    int i;
+    for (i = 0; i < 10; i++) {
+        printf("Digite o %2do. nome: ", i+1);
+        fflush(stdin); fgets(nomes[i], 40, stdin);
+    }
+}
+
+void ordenaNomes(char nomes[10][40]) {
+    int i, j;
+    char temp[40];
+    for (i = 0; i < 9; i++)
+        for (j = i+1; j < 10; j++)
+            if (strcmp(nomes[i], nomes[j]) > 0) {
+                strcpy(temp, nomes[i]);
+                strcpy(nomes[i], nomes[j]);
+                strcpy(nomes[j], temp);
+            }
+}
+
+void apresentaNomes(char nomes[10][40]) {
+    int i;
+    printf("\n");
+    for (i = 0; i < 10; i++)
+        printf("Nome: %2d --> %s", i+1, nomes[i]);
+}
+
+char perguntaOrdenacao() {
+    char op;
+    printf("\nVoce deseja organizar os nomes em ordem alfabetica? Sim[S] / Nao[N]: ");
+    fflush(stdin); op = getchar();
+    while (getchar() != '\n' && !feof(stdin));
+    return op;
+}
+
+char perguntaContinua() {
+    char resp;
+    printf("\n\nContinua? Sim[S] / Nao[N]: ");
+    fflush(stdin); resp = getchar();
+    while (getchar() != '\n' && !feof(stdin));
+    return resp;
+}
+
+int pesquisaNome(char nomes[10][40], char pesq[40]) {
+    int i;
+    for (i = 0; i < 10; i++) {
+        if (strcmp(pesq, nomes[i]) == 0)
+            return i;
+    }
+    return -1;
+}
+
 int main(void)
 {
-  int I, J, ACHA;
-  char NOME[10][40], PESQ[40], RESP, X[40];
-  printf("\nPesquisa e Listagem sequencial de nomes\n\n");
+    char NOME[10][40], PESQ[40], RESP, OPCAO;
+    int pos;
+    printf("\nPesquisa e listagem sequencial de nomes\n\n");
 
-  /* Entrada dos dados */
+    entradaNomes(NOME);
 
-  for (I = 0; I <= 9; I++)
-    {
-      printf("Digite o %2do. nome: ", I+1);
-      fflush(stdin); fgets(NOME[I], 40, stdin);
+    OPCAO = perguntaOrdenacao();
+
+    if (OPCAO == 'S' || OPCAO == 's') {
+        ordenaNomes(NOME);
+        printf("\nNomes organizados em ordem alfabetica:\n");
+        apresentaNomes(NOME);
+        system("pause");
+    } else {
+        printf("\nO sistema utilizara os nomes organizados na ordem de lancamento original.\n");
     }
 
-  /* Ordenacao de strings */
+    RESP = 'S';
+    while (RESP == 'S' || RESP == 's') {
+        printf("\nDigite o nome que deseja ser pesquisado: ");
+        fflush(stdin); fgets(PESQ, 40, stdin);
 
-  for (I = 0; I <= 8; I++)
-    for (J = I+1; J <= 9; J++)
-      if (strcmp(NOME[I], NOME[J]) > 0)
-        {
-          strcpy(X, NOME[I]);
-          strcpy(NOME[I], NOME[J]);
-          strcpy(NOME[J], X);
-        }
+        pos = pesquisaNome(NOME, PESQ);
 
-  /* Apresentacao dos nomes */
-
-  printf("\n");
-  for (I = 0; I <= 9; I++)
-     printf("Nome: %2d --> %s", I+1, NOME[I]);
-     system("pause");
-
-  /* Processamento da pesquisa enquanto RESP = S */
-
-  RESP = 'S';
-  while (RESP == 'S' || RESP == 's')
-    {
-      printf("\nEntre o nome a ser pesquisado: "); 
-      fflush(stdin); fgets(PESQ, 40, stdin);
-      I = 0;
-      ACHA = 0;
-      while (I <= 9 && ACHA == 0)
-        if (strcmp(PESQ, NOME[I]) == 0)
-          ACHA = 1;
+        if (pos != -1)
+            printf("%s foi localizado na posicao %d", PESQ, pos+1);
         else
-          I += 1;
-      if (ACHA == 1) 
-        printf("%s foi localizado na posicao %d",PESQ, I+1);
-      else
-        printf("%s nao foi localizado", PESQ);
-      printf("\n\nContinua? [S]IM/[N]AO + <Enter>: "); 
-      fflush(stdin); RESP = getchar();
+            printf("%s nao foi localizado", PESQ);
+
+        RESP = perguntaContinua();
     }
-  return(0);
+    return 0;
 }
