d8ed8fb20292819f16609ea0efc8fa8811bd5a73
Thiago Dias <thiagofellipe055@gmail.com>
2025-06-07 11:14:14 -0300

    Primeiros projetos

---
+++ mainFinal.c
@@ -0,0 +1,59 @@
+/* Pesquisa */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(void)
+{
+  int I, J, ACHA;
+  char NOME[10][40], PESQ[40], RESP, X[40];
+  printf("\nPesquisa e Listagem sequencial de nomes\n\n");
+
+  /* Entrada dos dados */
+
+  for (I = 0; I <= 9; I++)
+    {
+      printf("Digite o %2do. nome: ", I+1);
+      fflush(stdin); fgets(NOME[I], 40, stdin);
+    }
+
+  /* Ordenacao de strings */
+
+  for (I = 0; I <= 8; I++)
+    for (J = I+1; J <= 9; J++)
+      if (strcmp(NOME[I], NOME[J]) > 0)
+        {
+          strcpy(X, NOME[I]);
+          strcpy(NOME[I], NOME[J]);
+          strcpy(NOME[J], X);
+        }
+
+  /* Apresentacao dos nomes */
+
+  printf("\n");
+  for (I = 0; I <= 9; I++)
+     printf("Nome: %2d --> %s", I+1, NOME[I]);
+     system("pause");
+
+  /* Processamento da pesquisa enquanto RESP = S */
+
+  RESP = 'S';
+  while (RESP == 'S' || RESP == 's')
+    {
+      printf("\nEntre o nome a ser pesquisado: "); 
+      fflush(stdin); fgets(PESQ, 40, stdin);
+      I = 0;
+      ACHA = 0;
+      while (I <= 9 && ACHA == 0)
+        if (strcmp(PESQ, NOME[I]) == 0)
+          ACHA = 1;
+        else
+          I += 1;
+      if (ACHA == 1) 
+        printf("%s foi localizado na posicao %d",PESQ, I+1);
+      else
+        printf("%s nao foi localizado", PESQ);
+      printf("\n\nContinua? [S]IM/[N]AO + <Enter>: "); 
+      fflush(stdin); RESP = getchar();
+    }
+  return(0);
+}
