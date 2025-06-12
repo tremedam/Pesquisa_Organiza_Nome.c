8343799644eba32561799439d869d4fe2cf2cff6
Thiago Dias <thiagofellipe055@gmail.com>
2025-06-12 11:36:39 -0300

    tratamento de erros do Cod

--- mainFinal.c
+++ mainFinal.c
@@ -6,18 +6,18 @@ int main(void)
 {
   int I, J, ACHA;
   char NOME[10][40], PESQ[40], RESP, X[40];
+
   printf("\nPesquisa e Listagem sequencial de nomes\n\n");
 
   /* Entrada dos dados */
-
   for (I = 0; I <= 9; I++)
     {
       printf("Digite o %2do. nome: ", I+1);
       fflush(stdin); fgets(NOME[I], 40, stdin);
+      NOME[I][strcspn(NOME[I], "\n")] = '\0'; // Remove o ENTER do final do nome
     }
 
   /* Ordenacao de strings */
-
   for (I = 0; I <= 8; I++)
     for (J = I+1; J <= 9; J++)
       if (strcmp(NOME[I], NOME[J]) > 0)
@@ -31,8 +31,9 @@ int main(void)
 
   printf("\n");
   for (I = 0; I <= 9; I++)
-     printf("Nome: %2d --> %s", I+1, NOME[I]);
-     system("pause");
+      printf("Nome: %2d --> %s\n", I+1, NOME[I]);
+  printf("\n"); // Linha extra após o último nome
+  system("pause");
 
   /* Processamento da pesquisa enquanto RESP = S */
 
@@ -41,6 +42,7 @@ int main(void)
     {
       printf("\nEntre o nome a ser pesquisado: "); 
       fflush(stdin); fgets(PESQ, 40, stdin);
+      PESQ[strcspn(PESQ, "\n")] = '\0'; // Remove o ENTER do final
       I = 0;
       ACHA = 0;
       while (I <= 9 && ACHA == 0)
@@ -49,11 +51,12 @@ int main(void)
         else
           I += 1;
       if (ACHA == 1) 
-        printf("%s foi localizado na posicao %d",PESQ, I+1);
+        printf("\n%s foi localizado na posicao %d",PESQ, I+1);
       else
-        printf("%s nao foi localizado", PESQ);
-      printf("\n\nContinua? [S]IM/[N]AO + <Enter>: "); 
+        printf("\n%s nao foi localizado", PESQ);
+      printf("\n\nContinua? SIM[S] / NAO[N] + <Enter>: "); 
       fflush(stdin); RESP = getchar();
+      while (getchar() != '\n'); // Limpa o buffer até o ENTER
     }
   return(0);
 }
